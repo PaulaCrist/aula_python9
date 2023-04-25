982a9ef0d35514b9f44127169a05cc0932a3e656
Paula Cristiane <128405171+PaulaCrist@users.noreply.github.com>
2023-04-24 21:25:27 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+matriz_3x3=[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+#imprimir casa lista
+print(matriz_3x3[0])
+print(matriz_3x3[1])
+print(matriz_3x3[2])
+#imprimir um item de lista
+print(matriz_3x3[0][1])
+print(matriz_3x3[1][1])
+print(matriz_3x3[2][1])
---
+++ randow.py
@@ -0,0 +1,5 @@
+import randam
+
+n = int(imput('digite a dimesao n da matriz'))
+m = int(imput('digite a dimesao m da matriz'))
+matriz - []
